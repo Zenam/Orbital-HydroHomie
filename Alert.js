0e64dfc67832d181a9a7bee33202ad31c6397188
zululim <65802052+zululim@users.noreply.github.com>
2020-06-29 22:35:50 +0800

    Add files via upload

--- Alert.js
+++ Alert.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, Dimensions, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { Vibration, Platform, ListItem, StyleSheet, Text, View, Image, TextInput, Dimensions, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import Expo, { Constants, Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
 import BlueButton from './BlueButton.js';
 import firebase from './firebaseDb.js';
+import TimePicker from 'react-native-simple-time-picker';
 
 export default class Alerts extends Component {
     state = {
         notif: '',
-        startTime: 0,
-        endTime: 0
+        startTimeHrs: 0,
+        startTimeMins: 0,
+        endTimeHrs: 0,
+        endTimeMins: 0,
+        expoToken: ''
     }
-    updateNotif = (newNotif) => {
-        this.setState({notif: newNotif});
+
+    getExpoToken() {
+        let token = Notifications.getExpoPushTokenAsync();
+        this.setState({
+            expoToken: token
+        })
     }
+
     render() {
-        const {notif, startTime, endTime} = this.state;
+        const { notif, startTimeHrs, startTimeMins, endTimeHrs, endTimeMins, expoToken } = this.state
         return (
             <SafeAreaView style = {styles.container}>
                 <Text style = {styles.header}>
@@ -27,18 +38,53 @@ export default class Alerts extends Component {
                     onChangeText = {this.updateNotif}
                     value = {notif}
                 />
+                <Text style = {styles.subHeader}>
+                    Enter the window you would like to receive notifications from
+                </Text>
+                <Text style = {styles.subSubHeader}>
+                    From:
+                </Text>
+                <TimePicker
+                    startTimeHrs = {startTimeHrs}
+                    startTimeMins = {startTimeMins}
+                    onChange = {(hours, minutes) => {
+                        this.setState({
+                            startTimeHrs: hours,
+                            startTimeMins: minutes
+                        })
+                    }}
+                />
+                <Text style = {styles.subSubHeader}>
+                    To:
+                </Text>
+                <TimePicker
+                    endTimeHrs = {endTimeHrs}
+                    endTimeMins = {endTimeMins}
+                    onChange = {(hours, minutes) => {
+                        this.setState({
+                            endTimeHrs: hours,
+                            endTimeMins: minutes
+                        })
+                    }}
+                />
                 <BlueButton onPress = {() => {
-                    var currentUser = firebase.auth().currentUser.uid;
+                    var currentUser = firebase.auth().currentUser;
+                    this.getExpoToken();
                     firebase.firestore()
                         .collection('users')
-                        .doc(currentUser)
+                        .doc(currentUser.uid)
                         .update({
                             NotifInfo: {
-                                Notif: notif
+                                Title: 'Remember to drink up!',
+                                Token: expoToken,
+                                Notif: notif,
+                                startTimeHrs: startTimeHrs,
+                                startTimeMins: startTimeMins,
+                                endTimeHrs: endTimeHrs,
+                                endTimeMins: endTimeMins
                             }
                         });
-                }
-                }>
+                }}>
                     <Text>
                         Save
                     </Text>
@@ -73,6 +119,13 @@ const styles = StyleSheet.create({
         fontSize: 15,
         textAlign: 'center',
         marginTop: Dimensions.get('window').height*0.015
+    },
+    subSubHeader: {
+        color: 'black',
+        fontSize: 13,
+        textAlign: 'center',
+        marginTop: Dimensions.get('window').height*0.01,
+        marginBottom: Dimensions.get('window').height*0.01
     },
     textInput: {
         color: 'black',
