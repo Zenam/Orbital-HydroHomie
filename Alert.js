43daa2ec3de3a6217d2ab887c0856e614156483d
zululim <65802052+zululim@users.noreply.github.com>
2020-07-18 13:50:18 +0800

    Add files via upload

--- Alert.js
+++ Alert.js
@@ -2,31 +2,65 @@ import React, { Component } from 'react';
 import { Vibration, Platform, ListItem, StyleSheet, Text, View, Image, TextInput, Dimensions, SafeAreaView, KeyboardAvoidingView } from 'react-native';
 import Expo, { Constants, Notifications } from 'expo';
 import * as Permissions from 'expo-permissions';
-import BlueButton from './BlueButton.js';
-import firebase from './firebaseDb.js';
+import BlueButton from '../Components/BlueButton.js';
+import firebase from '../firebaseDb.js';
 import TimePicker from 'react-native-simple-time-picker';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default class Alerts extends Component {
     state = {
         notif: '',
-        startTimeHrs: 0,
-        startTimeMins: 0,
-        endTimeHrs: 0,
-        endTimeMins: 0,
-        expoToken: ''
+        expoToken: '',
+        time: 0
     }
 
-    /*
-    getExpoToken() {
-        let token = Notifications.getExpoPushTokenAsync();
+    componentDidMount() {
+        this.registerForPushNotificationsAsync();
+    }
+
+    registerForPushNotificationsAsync = async () => {
+        // check for existing permissions
+        const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+        let finalStatus = existingStatus;
+
+        // if no existing permission, ask useer for permission
+        if (existingStatus !== 'granted') {
+            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+            finalStatus = status;
+        }
+        // if no permission, exit function
+        if (finalStatus !== 'granted') {
+            return;
+        }
+        // get push notification token
+        let token = await Notifications.getExpoPushTokenAsync();
         this.setState({
             expoToken: token
         })
+    };
+
+    sendPushNotification = () => {
+        // I got the user that we will send the push notification to from the database and set it to state, now I have access to the users push token.
+        const userExpoToken = this.state.expoToken
+        // Now we will send the message to the expo servers
+        let response = fetch('https://exp.host/--/api/v2/push/send', {
+            method:'POST',
+            headers:{
+                Accept: 'application/json',
+                'Content-Type':'application/json',
+            },
+                body:JSON.stringify({
+                    to: userExpoToken,
+                    sound:'default',
+                    title: 'Remember to drink up!',
+                    body: this.state.notif
+                }
+            )
+        })
     }
-    */
 
     render() {
-        const { notif, startTimeHrs, startTimeMins, endTimeHrs, endTimeMins, expoToken } = this.state
+        const { notif, expoToken, time } = this.state
         return (
             <SafeAreaView style = {styles.container}>
                 <Text style = {styles.header}>
@@ -37,57 +71,47 @@ export default class Alerts extends Component {
                 </Text>
                 <TextInput
                     style = {styles.textInput}
-                    onChangeText = {this.updateNotif}
+                    onChangeText = {notif => this.setState({ notif })}
                     value = {notif}
                 />
                 <Text style = {styles.subHeader}>
-                    Enter the window you would like to receive notifications from
+                    Select the frequency at which you would like to receive reminders:
                 </Text>
-                <Text style = {styles.subSubHeader}>
-                    From:
-                </Text>
-                <TimePicker
-                    startTimeHrs = {startTimeHrs}
-                    startTimeMins = {startTimeMins}
-                    onChange = {(hours, minutes) => {
-                        this.setState({
-                            startTimeHrs: hours,
-                            startTimeMins: minutes
-                        })
+                <DropDownPicker
+                    items = {[
+                        {label: '1', value: 1},
+                        {label: '2', value: 2},
+                        {label: '3', value: 3},
+                        {label: '4', value: 4},
+                        {label: '5', value: 5},
+                    ]}
+                    defaultValue = {this.state.time}
+                    containerStyle = {{height: 40}}
+                    style = {{backgroundColor: '#fafafa'}}
+                    itemStyle = {{
+                        justifyContent: 'flex-start'
                     }}
-                />
-                <Text style = {styles.subSubHeader}>
-                    To:
-                </Text>
-                <TimePicker
-                    endTimeHrs = {endTimeHrs}
-                    endTimeMins = {endTimeMins}
-                    onChange = {(hours, minutes) => {
-                        this.setState({
-                            endTimeHrs: hours,
-                            endTimeMins: minutes
-                        })
-                    }}
+                    dropDownStyle = {{backgroundColor: '#fafafa'}}
+                    onChangeItem = {item => this.setState({
+                        time: item.value
+                    })}
                 />
                 <BlueButton onPress = {() => {
                     var currentUser = firebase.auth().currentUser;
-                    // this.getExpoToken();
                     firebase.firestore()
                         .collection('users')
                         .doc(currentUser.uid)
                         .update({
                             NotifInfo: {
                                 Title: 'Remember to drink up!',
-                                Token: expoToken,
                                 Notif: notif,
-                                startTimeHrs: startTimeHrs,
-                                startTimeMins: startTimeMins,
-                                endTimeHrs: endTimeHrs,
-                                endTimeMins: endTimeMins
+                                expoToken: expoToken,
+                                notifFrequency: time
                             }
                         });
+                    setInterval(() => {this.sendPushNotification();}, this.state.time*60*60*1000);
                 }}>
-                    <Text>
+                    <Text style = {styles.buttonText}>
                         Save
                     </Text>
                 </BlueButton>
@@ -137,6 +161,11 @@ const styles = StyleSheet.create({
         borderColor: 'skyblue',
         borderWidth: 1,
         borderRadius: 8,
+        textAlign: 'center'
+    },
+    buttonText: {
+        fontSize: 15,
+        color: 'white',
         textAlign: 'center'
     }
 })
